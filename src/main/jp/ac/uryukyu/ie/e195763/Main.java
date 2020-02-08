aa0668ae8c4bbea22ed41f665e06d94fb50e7efe
Riku Ohshiro <e195763@ie.u-ryukyu.ac.jp>
2020-02-09 06:35:42 +0900

    実行

--- src/main/jp/ac/uryukyu/ie/e195763/Main.java
+++ src/main/jp/ac/uryukyu/ie/e195763/Main.java
@@ -9,8 +9,35 @@ import static jp.ac.uryukyu.ie.e195763.Search.isMatch;
 public class Main {
     public static void main(String args[]) {
 
+        String[] dataset = {"gori", "ice", "easy", "egg", "apple"};
+        ArrayList<String> list = new ArrayList<>();
+        ArrayList<ArrayList<String>> result = new ArrayList<>();
+        ArrayList<ArrayList<String>> resclone = new ArrayList<>();
+        ArrayList<ArrayList<String>> resclone1 = new ArrayList<>();
+        ArrayList<ArrayList<String>> resclone2 = new ArrayList<>();
+        for (int i = 0; i < dataset.length; i++) {
+            list.add(dataset[i]);
+        }
+        for (int i = 0; i < list.size(); i++) {
+            for (int j = 0; j < list.size(); j++) {
+                if (isMatch(list.get(j).charAt(0), list.get(i).charAt(list.get(i).length() - 1))) {
+                    ArrayList<String> resultSub = new ArrayList<>();
+                    resultSub.add(list.get(i));
+                    resultSub.add(list.get(j));
+                    if(list.get(i).equals(list.get(j))){
+                    }else{
+                        result.add(resultSub);
+                    }
+                }
+            }
+        }
 
+        make(result, result, resclone);
+        make(result, resclone, resclone1);
+        make(result, resclone1, resclone2);
 
+        maxsearch(resclone2);
+        System.out.println(resclone2);
 
     }
 }
--- src/test/jp/ac/uryukyu/ie/e195763/SearchTest.java
+++ src/test/jp/ac/uryukyu/ie/e195763/SearchTest.java
@@ -1,27 +1,23 @@
 package jp.ac.uryukyu.ie.e195763;
 
-import org.junit.jupiter.api.Test;
-
-import java.util.ArrayList;
-
 class SearchTest {
 
-    @Test
-        void maxsearch(ArrayList<ArrayList<String>> array) {
+//    @Test
+//        void maxsearch(ArrayList<ArrayList<String>> array) {
+//
+//        int max = 0;
+//        int num = 0;
+//        for (int i = 0; i < array.size(); i++) {
+//            if (max < array.get(i).size()) {
+//                max = array.get(i).size();
+//                num = i;
+//            }
+//        }
+//        System.out.print("開始");
+//        for (int i = 0; i < array.get(num).size(); i++) {
+//            System.out.printf("->%s", array.get(num).get(i));
+//        }
+//        System.out.println("->終了");
+//    }
 
-        int max = 0;
-        int num = 0;
-        for (int i = 0; i < array.size(); i++) {
-            if (max < array.get(i).size()) {
-                max = array.get(i).size();
-                num = i;
-            }
-        }
-        System.out.print("開始");
-        for (int i = 0; i < array.get(num).size(); i++) {
-            System.out.printf("->%s", array.get(num).get(i));
-        }
-        System.out.println("->終了");
-    }
-    
 }
