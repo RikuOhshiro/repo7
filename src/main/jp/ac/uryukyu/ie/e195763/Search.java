cb7a2b1a22cc97ee8fcdccb4e5ac30e9efac03fe
Riku Ohshiro <e195763@ie.u-ryukyu.ac.jp>
2020-02-09 06:07:26 +0900

    1

---
+++ src/main/jp/ac/uryukyu/ie/e195763/Search.java
@@ -0,0 +1,22 @@
+package jp.ac.uryukyu.ie.e195763;
+
+public class Search {
+
+    boolean isMatch(char char1,char char2) {
+        char c1 = 'A';
+        char c2 = 'B';
+
+        if(char1 == char2){
+            return true;
+        }else {
+            return false;
+        }
+
+    }
+
+
+
+
+}
+
+
---
+++ src/test/jp/ac/uryukyu/ie/e195763/SearchTest.java
@@ -0,0 +1,18 @@
+package jp.ac.uryukyu.ie.e195763;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SearchTest {
+
+    @Test
+    boolean isMatch(char char1, char char2) {
+        if (char1 == char2) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+}
